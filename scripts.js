0b731b113a050ddb77f6784625c33f11ba18ef5c
Danilo Martinez <104922291+DaniloMartinezz@users.noreply.github.com>
2022-06-07 23:22:45 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const textoInicial = document.querySelector("#guto");
+const textoRestante = document.querySelector("#veiga");
+
+
+function expandirTexto() {
+    textoRestante.classList.toggle("active");
+
+    console.log("SEXOMORTAL");
+}
+
+
